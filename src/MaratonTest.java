9cda7504d55ae3dd338a1a492d73b84a349364c2
Emin <65346621+eminkrky@users.noreply.github.com>
2024-03-04 16:59:02 +0300

    Update MaratonTest.java

--- src/MaratonTest.java
+++ src/MaratonTest.java
@@ -1,5 +1,16 @@
-public class Main {
+public class MaratonTest {
     public static void main(String[] args) {
-        System.out.println("Hello world!");
+
+        String[] isimler = {"Kadir", "Gökhan", "Hakan", "Suzan", "Pınar", "Mehmet", "Ali", "Emel"
+                , "Fırat", "James", "Jale", "Ersin", "Deniz", "Gözde", "Anıl", "Burak"};
+
+        int[] sure = {341, 273, 278, 329, 445, 402, 388, 270, 243, 334, 412, 393, 299, 343, 317, 265};
+
+        Maraton maraton = new Maraton(sure,isimler);
+
+        maraton.Ucuncu();
+
+        maraton.ogrencileriSiniflandir();
+
     }
 }
