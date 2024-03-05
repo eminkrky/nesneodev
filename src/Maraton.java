e9a16e5a610691d6846310d603727714ed4d040b
Emin <65346621+eminkrky@users.noreply.github.com>
2024-03-05 17:14:21 +0300

    Update Maraton.java

--- src/Maraton.java
+++ src/Maraton.java
@@ -1,71 +1,50 @@
 public class Maraton {
-    private final int[] zamanlar;
-    private final String[] isimler;
+    public String[] isimler;
+    public int[] sureler;
 
-    public Maraton(int[] zamanlar, String[] isimler) {
-        this.zamanlar = zamanlar;
+    public Maraton(String[] isimler , int[] sureler) {
         this.isimler = isimler;
+        this.sureler = sureler;
     }
 
-    private int Birinci() {
+    public int getbirinci() {
         int min = 0;
-        for (int i = 1; i < zamanlar.length; i++) {
-            if (zamanlar[i] < zamanlar[min]) {
+        for (int i = 0; i < sureler.length; i++) {
+            if (sureler[i]<sureler[min])
                 min = i;
-            }
         }
-        System.out.printf("Birinci: %s %d dakika%n", isimler[min], zamanlar[min]);
+        System.out.printf("Birinci: %s %d %n",isimler[min],sureler[min]);
         return min;
     }
-
-    public int Ikinci(String[] isimler) {
-        int enIyiDereceIndis = Birinci();
-        int ikinciEnIyi = -1;
-
-        for (int i = 0; i < zamanlar.length; i++) {
-            if (i != enIyiDereceIndis && (ikinciEnIyi == -1 || zamanlar[i] < zamanlar[ikinciEnIyi])) {
-                ikinciEnIyi = i;
-            }
-        }
-        System.out.printf("İkinci : %s %d dakika%n", isimler[ikinciEnIyi], zamanlar[ikinciEnIyi]);
-        return ikinciEnIyi;
-    }
-
-
-    public void Ucuncu() {
-        int enIyiIndis = Ikinci(isimler);
-        int min = -1;
-        for (int i = 0; i < zamanlar.length; i++) {
-            if (i != enIyiIndis && (min == -1 || zamanlar[i] < zamanlar[min])) {
+    public int getikinci(int birinci) {
+       int min = -1;
+       for (int i = 0; i < sureler.length; i++) {
+            if (i!=birinci&&(min==-1||sureler[i]<sureler[min]))
                 min = i;
-            }
         }
-        int ikinciEnIyi = min;
-        int ucuncuEnIyi = -1;
-
-        for (int i = 0; i < zamanlar.length; i++) {
-            if (i != enIyiIndis && i != ikinciEnIyi && (ucuncuEnIyi == -1 || zamanlar[i] < zamanlar[ucuncuEnIyi])) {
-                ucuncuEnIyi = i;
-            }
-        }
-
-        System.out.printf("Üçüncü : %s  %d dakika%n", isimler[ucuncuEnIyi], zamanlar[ucuncuEnIyi]);
+        System.out.printf("İkinci: %s %d %n",isimler[min],sureler[min]);
+        return min;
     }
-
-    public void ogrencileriSiniflandir() {
-        int[] siniflandirma = new int[3];
-
-        for (int j : zamanlar) {
-            if (j >= 200 && j <= 299) {
-                siniflandirma[0]++;
-            } else if (j >= 300 && j <= 399) {
-                siniflandirma[1]++;
-            } else {
-                siniflandirma[2]++;
-            }
+    public int getucuncu(int birinci,int ikinci) {
+        int min = -1;
+        for (int i = 0; i < sureler.length; i++) {
+            if (i!=birinci&&(i!=ikinci&&(min==-1||sureler[i]<sureler[min])))
+                min = i;
         }
-        System.out.println("A -> " + siniflandirma[0]);
-        System.out.println("B -> " + siniflandirma[1]);
-        System.out.println("C -> " + siniflandirma[2]);
+        System.out.printf("Üçüncü: %s %d %n",isimler[min],sureler[min]);
+        return min;
     }
+    public void getsininflandir(){
+        int a=0;
+        int b=0;
+        int c=0;
+        for (int i = 0; i < sureler.length; i++) {
+            if (sureler[i]>=200&&sureler[i]<=299)
+                a++;
+            else if (sureler[i]>=300&&sureler[i]<=399)
+                b++;
+               else c++;
+            }
+        System.out.printf("A -> %s%nB -> %s%nC -> %s",a,b,c);
+        }
 }
